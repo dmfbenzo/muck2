a391ca0d39411c02cee916e0b1014ceae8a2b4ea
dmfbenzo <dmfbenzo@gmail.com>
2022-12-08 23:50:02 -0600

    Add files via upload

---
+++ muck/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  muck
+//
+//  Created by nicholas p wilde on 12/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ muck/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  muck
+//
+//  Created by nicholas p wilde on 12/8/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
