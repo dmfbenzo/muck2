214d58f9ca51b9ae0d3a29c2492cea06dab05b95
dmfbenzo <dmfbenzo@gmail.com>
2022-12-08 23:50:58 -0600

    Add files via upload

---
+++ muck2/muck/muck/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  muck
+//
+//  Created by nicholas p wilde on 12/8/22.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
